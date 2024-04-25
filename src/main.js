182ca67b3605cdeab509aa9c29057b413dba9d24
DanielRumay <165932669+DanielRumay@users.noreply.github.com>
2024-04-25 01:10:50 +0000

    laboratorio

--- src/main.js
+++ src/main.js
@@ -1,19 +1,16 @@
-import './style.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 const Main = () => {
     return (
         <main>
             <div id="carouselExample" className="carousel slide">
                 <div className="carousel-inner">
                     <div className="carousel-item active">
-                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrMbPG4cFXPXBbkxIPcAJskY7sF679oGj5dbq4K_YCWw&s" className="d-block w-100 carousel-image" alt="..." />
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrMbPG4cFXPXBbkxIPcAJskY7sF679oGj5dbq4K_YCWw&s" className="d-block w-100" alt="..." />
                     </div>
                     <div className="carousel-item">
-                        <img src="https://explore.zoom.us/media/acc-feature01.png" className="d-block w-100 carousel-image" alt="..." />
+                        <img src="https://explore.zoom.us/media/acc-feature01.png" className="d-block w-100" alt="..." />
                     </div>
                     <div className="carousel-item">
-                        <img src="https://www.zoom.com/contentassets/fde9f1d3f5574877a5e58bb2ff94cca7/aic-meeting-questions-blog-image.jpg?t=20240416030002" className="d-block w-100 carousel-image" alt="..." />
+                        <img src="https://www.zoom.com/contentassets/fde9f1d3f5574877a5e58bb2ff94cca7/aic-meeting-questions-blog-image.jpg?t=20240416030002" className="d-block w-100" alt="..." />
                     </div>
                 </div>
                 <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
