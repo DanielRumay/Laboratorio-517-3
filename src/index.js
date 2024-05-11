d5791fa293fd7288340ab0ecfa2e07d23f4a32d7
DanielRumay <165932669+DanielRumay@users.noreply.github.com>
2024-05-11 04:22:12 +0000

    Laboratorio

--- src/index.js
+++ src/index.js
@@ -38,34 +38,3 @@ const root = createRoot(document.getElementById("root"));
 root.render(
     <RouterProvider router={router} />
 );
-/*import './style.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React, {StrictMode} from "react";
-import { createRoot } from "react-dom/client";
-import Message from "./message";
-import Header from "./header";
-import Main from "./main";
-import Aside from "./aside";
-import Footer from "./footer";
-
-
-const root = createRoot(document.getElementById("root"));
-root.render(
-    <StrictMode>
-        <Header></Header>
-        <div className="container">
-            <br/>
-            <div className="row">
-                <div className="col-md-8">
-                    <Main></Main>
-                </div>
-                <div className="col-md-4">
-                    <Aside></Aside>
-                </div>
-            </div>
-        </div>
-        <br/>
-        <br/>
-        <Footer></Footer>
-    </StrictMode>
-);*/
